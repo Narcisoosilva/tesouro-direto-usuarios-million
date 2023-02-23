95d296b69d688048c3e186c7ec8d6ab73abc969c
Narcisoosilva <narcisoosilva@gmail.com>
2023-02-22 21:27:14 -0300

    Colocando  variável de ambiente do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost/millionTesouroDireto';
+const uri = process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost/millionTesouroDireto';
 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
